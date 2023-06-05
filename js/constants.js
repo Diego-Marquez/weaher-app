07c6179ceeca181422b844fdf03f0aa788d18651
Diego Marquez <47926525+Diego-Marquez@users.noreply.github.com>
2023-06-05 14:45:00 -0300

    Add files via upload

---
+++ js/constants.js
@@ -0,0 +1,12 @@
+export const API_KEY = '159d7298aef942712b036dd99058a2eb'
+export const BASE_API = 'https://api.openweathermap.org/data/2.5/'
+
+export const weatherConditionsCodes = {
+  2: 'rainy',
+  3: 'drizzle',
+  5: 'rainy',
+  6: 'drizzle',
+  7: 'cloudy',
+  8: 'clean',
+
+}
